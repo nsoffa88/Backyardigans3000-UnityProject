0bf2f63610be6a2e7910ea005165d4da636f1daf
Nicholas Soffa <nicktsoffa@gmail.com>
2018-10-22 20:27:07 -0700

    Player and Camera Movement
    Player Movement with WASD is working. Camera is set up to be Isometric and follows the player.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 10f;
+
+    Vector3 movement;
+    Rigidbody playerRigidBody;
+
+    private void Awake()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidBody.MovePosition(transform.position + movement);
+    }
+}
