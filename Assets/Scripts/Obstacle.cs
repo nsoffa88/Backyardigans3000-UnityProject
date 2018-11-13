b13ebab26e2ca0a3524d71ff9f2762606d3c82d6
Nicholas Soffa <nicktsoffa@gmail.com>
2018-11-12 20:59:20 -0800

    Created a basic Health System
    Created a basic Player Health System, when player character collides with obstacle their health is reduced. Once health is reduced to 0 or below then player character is deleted.

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<PlayerHealth>();
+        if (health != null)
+        {
+            health.TakeDamage(50);
+            hit.transform.position = new Vector3(0, 1, 0);
+        }
+        if (health.currentHealth <= 0)
+        {
+            Destroy(hit);
+        }
+    }
+}
