d041779a5f2e8e19c9d56b61c5524d3397ff179e
Nicholas Soffa <nicktsoffa@gmail.com>
2019-03-12 19:06:18 -0700

    Player 3 Hit Combo
    Player basic 3 hit combo works for controller "x" button inputs. 3 simple animations for each swing, and will reset to resting position according to which animation just ended.

---
+++ Assets/Scripts/Animation/PlayerAttack1Behavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack1Behavior : StateMachineBehaviour {
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+        //PlayerAttack playerAttack = animator.GetComponent<PlayerAttack>();
+
+        //Debug.Log(playerAttack.basicComboCount);
+        /*if (playerAttack.basicComboCount >= 2) {
+            Debug.Log("Attack 2 Time");
+            animator.SetTrigger("Attack2");
+        }*/
+    }
+}
--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
 
-    public GameObject sword;
     private Animator swordAnimator;
+
+    int basicComboCount = 0;
+    int usedComboCount = 0;
+    float lastAttackTime = 1f;
+    float maxComboDelay = 1f;
 
     private void Start()
     {
-        swordAnimator = sword.GetComponent<Animator>();
+        swordAnimator = gameObject.GetComponent<Animator>();
     }
 
     void Update () {
+
+        if (Time.time - lastAttackTime > maxComboDelay && basicComboCount != 0) {
+            Debug.Log("reset now: " + Time.time + " Attack Time: " + lastAttackTime);
+            basicComboCount = 0;
+            swordAnimator.ResetTrigger("Attack");
+            swordAnimator.SetTrigger("Reset");
+        }
+
         if (Input.GetKeyDown(KeyCode.JoystickButton2))
         {
             PerformAttack();
@@ -21,7 +33,22 @@ public class PlayerAttack : MonoBehaviour {
 
     public void PerformAttack()
     {
-        //Attack Animations
-        swordAnimator.SetTrigger("BaseAttack");
+        if (basicComboCount != 4 && !swordAnimator.GetBool("Attack")) {
+            lastAttackTime = Time.time;
+            basicComboCount++;
+
+            //Attack Animations
+            Debug.Log("Attack Time: " + basicComboCount);
+            swordAnimator.SetTrigger("Attack");
+        } /* else {
+            swordAnimator.ResetTrigger("Attack");
+        }
+        //Debug.Log(basicComboCount);
+ 
+        //basicComboCount = Mathf.Clamp(basicComboCount, 0, 2);
+
+        if(basicComboCount == 3) {
+            swordAnimator.ResetTrigger("Attack");
+        }*/
     }
 }
